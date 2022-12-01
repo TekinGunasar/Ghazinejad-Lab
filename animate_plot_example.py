4ad99a77da0aa2944ba882cf47ec2652dc27ac6c
Tekin Gunasar <tgunasar@ucsd.edu>
2022-11-30 19:34:31 -0800

    Added videos and adjusted code for creating them

--- animate_plot_example.py
+++ animate_plot_example.py
@@ -2,34 +2,66 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imageio
 import os
+import pandas as pd
 
 from math import floor
+import json
 
-def create_graph_video(x,y,num_frames_per,frames_directory,final_directory):
 
-    y_max = max(y+30)
-    x_max = max(x+0.1)
-    for i in range(2, len(x)+1):
-        plt.scatter(x[0:i],y[0:i],c='purple')
-        plt.xlim(0,x_max)
-        plt.ylim(0, y_max)
-        plt.title('Tensile Stress v.s Tensile Strain - Aluminum')
-        plt.xlabel('Tensile Strain (Displacement) [%]')
-        plt.ylabel('Tensile Stress [MPa]')
-        plt.savefig(f'{frames_directory}\line-{i}.png')
-        plt.close()
+def create_graph_video(settings_file):
 
-    with imageio.get_writer('line.mov', mode='?') as writer:
-        for i in range(2, len(x)+1):
-            image = imageio.imread(f'{frames_directory}\line-{i}.png')
-            [writer.append_data(image) for iter in range(num_frames_per)]
+    settings = json.loads(open(settings_file,'r').read())['plot settings']
+    data = pd.read_csv(settings['csv_file_path'])
+
+    if not os.path.exists(settings['frames directory']):
+        os.mkdir(settings['frames directory'])
+
+    if not os.path.exists(settings['movie directory']):
+        os.mkdir(settings['movie directory'])
+
+    for frame in os.listdir(settings['frames directory']):
+        os.remove(os.path.join(settings['frames directory'],frame))
+
+    #Getting relevant data
+    force_data = pd.to_numeric(data['Force'][2:]).values
+    displacement_data = pd.to_numeric(data['Displacement'][2:]).values
+
+    strain_data = pd.to_numeric(data['Strain 1'][2:]).values
+    stress_data = force_data * float(settings['stress conversion factor'])
+
+    max_force = max(force_data) + int(max(force_data)/6)
+    max_stress = max(stress_data) + 40
+
+    num_data_points = len(force_data)
+    with imageio.get_writer(os.path.join(settings['movie directory'],settings['movie name']), mode='?') as writer:
+        for i in range(num_data_points):
+
+            fig, axs = plt.subplots(3, 1)
+            fig.suptitle(settings['material'])
+
+            axs[0].scatter(displacement_data[0:i], force_data[0:i], c='g')
+            axs[0].set_title('Force v.s Displacement')
+            axs[0].set_xlabel('Displacement(mm)')
+            axs[0].set_ylabel('Force (kN)')
+            axs[0].set_ylim(0,max_force)
+
+            axs[2].scatter(strain_data[0:i], stress_data[0:i], c='b')
+            axs[2].set_title('Stress v.s Strain')
+            axs[2].set_xlabel('Strain')
+            axs[2].set_ylabel('Stress (MPa)')
+            axs[2].set_ylim(0,max_stress)
+
+            fig.delaxes(axs[1])
+
+            plt.subplots_adjust(hspace= -0.05)
+            plt.savefig(os.path.join(settings['frames directory'],f'frame-{i}.png'))
+            plt.close()
+
+            image = imageio.imread(os.path.join(settings['frames directory'],f'frame-{i}.png'))
+            [writer.append_data(image) for iter in range(int(settings['num frames per']))]
 
 if __name__ == '__main__':
-    size = 100
+    settings = 'settings_files/brass_settings.json'
+    create_graph_video(settings)
 
-    x = np.arange(0,10,step=0.05)
-    y = [x_i**2 for x_i in x]
-    num_frames_per = 1
-    final_directory ='./'
-    frames_directory = 'Animation Frames\Aluminum'
-    create_graph_video(x,y,num_frames_per,frames_directory,final_directory)
+
