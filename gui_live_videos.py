ca5699cd739c121d99a6ba9d2f528aad54b4b5df
Tekin Gunasar <tgunasar@ucsd.edu>
2023-02-22 15:00:39 -0800

    gui

--- gui_live_videos.py
+++ gui_live_videos.py
@@ -2,15 +2,22 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 
 import numpy as np
-import time
-import os
-import cv2 as cv
 
-from threading import Thread,activeCount
+class Color(QWidget):
 
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+#Class making up the threading functionality needed to split into GUI thread and video frame processing thread
+
+#Inheriting from QThread, which directly inherits from default Python threading module
 class VideoThread(QThread):
    change_pixmap_signal = pyqtSignal(np.ndarray)
 
@@ -18,12 +25,14 @@ class VideoThread(QThread):
       super().__init__()
       self._run_flag = True
 
+   #overriding the 'run' function in default threading module, whenever thread.start() is called, run will be invoked
    def run(self):
       # capture from web cam
       cap = cv.VideoCapture('live_videos/HCS/HCS Graphic.mp4')
       while self._run_flag:
          ret, cv_img = cap.read()
          if ret:
+            #emitting signal, which is connected to the update_img func. to display the next video frame
             self.change_pixmap_signal.emit(cv_img)
       # shut down capture system
       cap.release()
@@ -33,96 +42,87 @@ class VideoThread(QThread):
       self._run_flag = False
       self.wait()
 
-
-
 class MainWindow(QMainWindow):
 
-   def __init__(self):
-      super(MainWindow, self).__init__()
-      self.setWindowTitle("Multimedia Lesson")
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.setWindowTitle('Multimedia Lesson')
 
-      self.display_width = 640
-      self.display_height = 480
+        self.main_layout = QHBoxLayout()
 
-      main_layout = QHBoxLayout()
-      video_selection = QVBoxLayout()
+        #Video Selection section
+        self.video_selection = QVBoxLayout()
+        self.main_layout.addLayout(self.video_selection,5)
 
-      groupbox_tensile_tests = QGroupBox('Tensile Test Videos')
-      groupbox_tensile_tests.setStyleSheet("background-color:rgb(98,178,232); font-size:100px; border: 1px solid black;")
+        '''Adding video selection section for the tensile tests
+        Involves a groupbox, and then setting the layout for that groupbox as another VBoxLayout
+        for the vertically stacked buttons for the six different materials'''
 
-      vbox_tensile_tests = QVBoxLayout()
-      groupbox_tensile_tests.setLayout(vbox_tensile_tests)
+        self.tensile_test_groupbox = QGroupBox('Tensile Test Videos')
+        self.tensile_test_vbox = QVBoxLayout()
+        self.tensile_test_groupbox.setLayout(self.tensile_test_vbox)
 
-      vid_tensile_1_button = QPushButton('Video 1')
-      vbox_tensile_tests.addWidget(vid_tensile_1_button)
-      vid_tensile_1_button.setStyleSheet('QPushButton {border: 5px solid black; background:white;} '
-                                         'QPushButton:hover {background:rgb(230,230,230)}')
+        #Now adding the groupbox as the first element in the video_selection vbox an styling it
+        self.video_selection.addWidget(self.tensile_test_groupbox)
+        self.tensile_test_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 1px solid black;")
 
-      groupbox_coldworks = QGroupBox('Coldworks Videos')
-      groupbox_coldworks.setContentsMargins(10,10,10,10)
-      groupbox_coldworks.setStyleSheet("background-color:rgb(98,178,232); font-size:100px; border: 1px solid black;")
-
-      vbox_coldworks = QVBoxLayout()
-      groupbox_coldworks.setLayout(vbox_coldworks)
-
-      vid_coldworks_1_button = QPushButton('Video 1')
-      vid_coldworks_1_button.setStyleSheet('border: 5px solid black; background:white;')
-      vbox_coldworks.addWidget(vid_coldworks_1_button)
-
-      video_selection.addWidget(groupbox_tensile_tests)
-      video_selection.addWidget(groupbox_coldworks)
-
-      main_layout.addLayout(video_selection)
+        #Adding the videos that make up the vbox layout of the tensile tests groupbox
+        self.tensile_test_v1 = QPushButton('Video 1')
+        self.tensile_test_vbox.addWidget(self.tensile_test_v1)
+        self.tensile_test_v1.setStyleSheet('QPushButton {border: 5px solid black; background:rgb(255,255,255);} '
+                                           'QPushButton:hover {background:rgb(180,180,180)}')
 
 
-      video_player = QGroupBox("Video will be Displayed Here")
-      video_player.setStyleSheet("font-size:50px;")
+        #Adding video selection section now for the coldworks videos in the same manner
+        self.coldworks_groupbox = QGroupBox('Coldworks Videos')
+        self.coldworks_vbox = QVBoxLayout()
+        self.coldworks_groupbox.setLayout(self.coldworks_vbox)
 
-      video_player.setStyleSheet(("background-color:white;"))
-      main_layout.addWidget(video_player,10)
+        self.video_selection.addWidget(self.coldworks_groupbox)
+        self.coldworks_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 1px solid black;")
 
-      video_player_vbox = QVBoxLayout()
+        #Ading the videos that make up the vbox layout of the coldworks groupbox
+        self.coldworks_v1 = QPushButton('Video 1')
+        self.coldworks_vbox.addWidget(self.coldworks_v1)
+        self.coldworks_v1.setStyleSheet('QPushButton {border: 5px solid black; background:rgb(255,255,255);} '
+                                           'QPushButton:hover {background:rgb(180,180,180)}')
 
-      self.image_label = QLabel(self)
-      video_player_vbox.addWidget(self.image_label,10)
+        #Video player section
+        self.video_player = QVBoxLayout()
+        self.video_player.addWidget(Color('green'))
+        self.main_layout.addLayout(self.video_player,10)
 
-      video_player.setLayout(video_player_vbox)
+        self.centralWidget = QWidget()
+        self.centralWidget.setLayout(self.main_layout)
 
-      self.thread = VideoThread()
+        #Video frames stored as member proprty of this image_label variable as a Pixmap
+        self.image_label = QLabel(self)
 
-      self.thread.change_pixmap_signal.connect(self.update_image)
+        self.video_player.addWidget(self.image_label)
 
-      self.thread.start()
+         #Cannot display GUI elements and perform video frame processing to display on GUI
+         #on the same GUI thread. Below code is using the above VideoThread class to implement
+         #multithreading functionality to expand thread pool into GUI thread and video frame processing thread.
+         
+         #Much more memory efficient and performant than traditional programming.
 
-      widget = QWidget()
-      widget.setLayout(main_layout)
+        #Instantiating thread
+        #self.thread = VideoThread()
 
-      self.setCentralWidget(widget)
+        #Whenver a 'signal' is emitted, i.e a new frame is loaded into the video, the 'update_image' function is called
+        #Logic for this implemented in 'run' function in VideoThread
+        #self.thread.change_pixmap_signal.connect(self.update_image)
 
-   def closeEvent(self, event):
-      self.thread.stop()
-      event.accept()
+        #invokes 'run' function in video thread for this instance
+        #self.thread.start()
 
-   @pyqtSlot(np.ndarray)
-   def update_image(self, cv_img):
-      qt_img = self.convert_cv_qt(cv_img)
-      self.image_label.setPixmap(qt_img)
-
-   def convert_cv_qt(self, cv_img):
-      rgb_image = cv.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-      h, w, ch = rgb_image.shape
-      bytes_per_line = ch * w
-      convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-      p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
-      return QPixmap.fromImage(p)
-
+        self.setCentralWidget(self.centralWidget)
 
 def main():
-   app = QApplication(sys.argv)
-   ex = MainWindow()
-   ex.show()
-   sys.exit(app.exec_())
-
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.showMaximized()
+    app.exec()
 
 if __name__ == '__main__':
-   main()
+    main()
