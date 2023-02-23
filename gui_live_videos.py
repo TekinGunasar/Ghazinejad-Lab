db3061d4085d6dfde0924635a727ea10daeb4dd9
Tekin Gunasar <tgunasar@ucsd.edu>
2023-02-22 21:09:08 -0800

    fixed vid display

--- gui_live_videos.py
+++ gui_live_videos.py
@@ -3,7 +3,10 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+
 import numpy as np
+
+import cv2 as cv
 
 class Color(QWidget):
 
@@ -28,8 +31,10 @@ class VideoThread(QThread):
    #overriding the 'run' function in default threading module, whenever thread.start() is called, run will be invoked
    def run(self):
       # capture from web cam
-      cap = cv.VideoCapture('live_videos/HCS/HCS Graphic.mp4')
+      cap = cv.VideoCapture(0)
+
       while self._run_flag:
+
          ret, cv_img = cap.read()
          if ret:
             #emitting signal, which is connected to the update_img func. to display the next video frame
@@ -50,6 +55,9 @@ class MainWindow(QMainWindow):
 
         self.main_layout = QHBoxLayout()
 
+        self.display_width = 1000
+        self.display_height = 1000
+
         #Video Selection section
         self.video_selection = QVBoxLayout()
         self.main_layout.addLayout(self.video_selection,5)
@@ -64,7 +72,7 @@ class MainWindow(QMainWindow):
 
         #Now adding the groupbox as the first element in the video_selection vbox an styling it
         self.video_selection.addWidget(self.tensile_test_groupbox)
-        self.tensile_test_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 1px solid black;")
+        self.tensile_test_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 5px solid black;")
 
         #Adding the videos that make up the vbox layout of the tensile tests groupbox
         self.tensile_test_v1 = QPushButton('Video 1')
@@ -72,14 +80,13 @@ class MainWindow(QMainWindow):
         self.tensile_test_v1.setStyleSheet('QPushButton {border: 5px solid black; background:rgb(255,255,255);} '
                                            'QPushButton:hover {background:rgb(180,180,180)}')
 
-
         #Adding video selection section now for the coldworks videos in the same manner
         self.coldworks_groupbox = QGroupBox('Coldworks Videos')
         self.coldworks_vbox = QVBoxLayout()
         self.coldworks_groupbox.setLayout(self.coldworks_vbox)
 
         self.video_selection.addWidget(self.coldworks_groupbox)
-        self.coldworks_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 1px solid black;")
+        self.coldworks_groupbox.setStyleSheet("background-color:rgb(98,178,232); font-size:50px; border: 5px solid black;")
 
         #Ading the videos that make up the vbox layout of the coldworks groupbox
         self.coldworks_v1 = QPushButton('Video 1')
@@ -89,39 +96,67 @@ class MainWindow(QMainWindow):
 
         #Video player section
         self.video_player = QVBoxLayout()
-        self.video_player.addWidget(Color('green'))
-        self.main_layout.addLayout(self.video_player,10)
+
+        #Making sure all the video frames will be in just on groupbox within the video player vbox
+        self.video_player_groupbox = QGroupBox('Video will be displayed here')
+        self.video_player_groupbox_vbox = QVBoxLayout()
+        self.video_player_groupbox.setLayout(self.video_player_groupbox_vbox)
+
+        self.video_player_groupbox.setStyleSheet('background-color:green; font-size:25px;')
+
+        self.video_player.addWidget(self.video_player_groupbox)
+
+        self.image_label = QLabel(self)
+
+        self.video_player_groupbox_vbox.addWidget(self.image_label)
+
+        self.main_layout.addLayout(self.video_player, 10)
+
+        # Video frames stored as member proprty of this image_label variable as a Pixmap
+
+        # Cannot display GUI elements and perform video frame processing to display on GUI
+        # on the same GUI thread. Below code is using the above VideoThread class to implement
+        # multithreading functionality to expand thread pool into GUI thread and video frame processing thread.
+
+        # Much more memory efficient and performant than traditional programming.
+
+        # Instantiating thread
+        self.thread = VideoThread()
+
+        # Whenver a 'signal' is emitted, i.e a new frame is loaded into the video, the 'update_image' function is called
+        # Logic for this implemented in 'run' function in VideoThread
+        self.thread.change_pixmap_signal.connect(self.update_image)
+
+        # invokes 'run' function in video thread for this instance
+        self.thread.start()
 
         self.centralWidget = QWidget()
         self.centralWidget.setLayout(self.main_layout)
-
-        #Video frames stored as member proprty of this image_label variable as a Pixmap
-        self.image_label = QLabel(self)
-
-        self.video_player.addWidget(self.image_label)
-
-         #Cannot display GUI elements and perform video frame processing to display on GUI
-         #on the same GUI thread. Below code is using the above VideoThread class to implement
-         #multithreading functionality to expand thread pool into GUI thread and video frame processing thread.
-         
-         #Much more memory efficient and performant than traditional programming.
-
-        #Instantiating thread
-        #self.thread = VideoThread()
-
-        #Whenver a 'signal' is emitted, i.e a new frame is loaded into the video, the 'update_image' function is called
-        #Logic for this implemented in 'run' function in VideoThread
-        #self.thread.change_pixmap_signal.connect(self.update_image)
-
-        #invokes 'run' function in video thread for this instance
-        #self.thread.start()
-
         self.setCentralWidget(self.centralWidget)
+
+        def closeEvent(self, event):
+           self.thread.stop()
+           event.accept()
+
+    @pyqtSlot(np.ndarray)
+    def update_image(self, cv_img):
+       """Updates the image_label with a new opencv image"""
+       qt_img = self.convert_cv_qt(cv_img)
+       self.image_label.setPixmap(qt_img)
+
+    def convert_cv_qt(self, cv_img):
+       """Convert from an opencv image to QPixmap"""
+       rgb_image = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
+       h, w, ch = rgb_image.shape
+       bytes_per_line = ch * w
+       convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+       p = convert_to_Qt_format.scaled(self.display_width, self.display_height, Qt.KeepAspectRatio)
+       return QPixmap.fromImage(p)
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.showMaximized()
+    window.showFullScreen()
     app.exec()
 
 if __name__ == '__main__':
